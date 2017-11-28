2cbc16d891ae7fc14d87a2e8f6fdfd44ff813f1f
hanum.lee <chococloudy@gmail.com>
2017-11-28 12:48:11 -0700

    VideoCall has been fixed so it now uses WpfApp1 namesapce

--- WpfApp1/Videocall.xaml.cs
+++ WpfApp1/Videocall.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CPCS481Project
+namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class VideoCall : Window
     {
-        public MainWindow()
+        public VideoCall()
         {
             InitializeComponent();
         }
