5c9cbe8da85c98c10b0a6523b36f83c17ea53265
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2017-11-30 13:25:07 -0700

    added more variables to Global_Data

--- WpfApp1/Global_Data.cs
+++ WpfApp1/Global_Data.cs
@@ -11,6 +11,9 @@ namespace WpfApp1
     {
         public static string[] existing_Usernames = new string[100]; //{ "liwanisz", "Lillian", "cora", "Cora", "Cora1" };
         public static string[] passwords = new string[100]; //{ "1234", "3245", "0000", "1111", "3333" };
+        public static string currentUser = "Cora1"; //hard coded... change later
+        public static string[] profilePicPath = new string[100]; //paths to everyone's profile pictures
+        public static string[,] contacts = new string[100, 100]; //2d arrays in c# are different than Java, FYI
 
     }
 
