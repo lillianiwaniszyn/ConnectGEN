4157bea90d58e25a9bfa750e412d9ee1093b22d7
KarlaMariann Juego <karlamariann.juego@ucalgary.ca>
2017-12-03 21:51:26 -0700

    Successfully created TranslateButton_Click (Some bugs remain unfixed...but works.)

--- WpfApp1/TranslationMenu.xaml.cs
+++ WpfApp1/TranslationMenu.xaml.cs
@@ -31,5 +31,123 @@ namespace WpfApp1
             // Use the MessageScreen to enable buttons.
             oldWindow.EnableAllButtons();
         }
+
+        private void TranslateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get languages from the menus.
+            string initialLanguage = initialLanguageMenu.SelectedValue.ToString();
+            string finalLanguage = finalLanguageMenu.SelectedValue.ToString();
+            // Get message that needs to be translated.
+            string toBeTranslated = initialMessageBox.Text;
+            string translatedMessage = "";
+
+            if (initialLanguage == finalLanguage)
+            {
+                translatedMessage = toBeTranslated;
+            }
+            else if (initialLanguage == "English")
+            {
+                // If so, call the final language's text file.
+                // Use the English translation to get the translation.
+
+                translatedMessage = FromEnglishTranslation(toBeTranslated, finalLanguage);
+            }
+            else if (finalLanguage == "English")
+            {
+                // Need to call the initial language's text file.
+                translatedMessage = ToEnglishTranslation(toBeTranslated, initialLanguage);
+            }
+            else
+            {
+                // Otherwise, need to call the initial language's text file to obtain the English translation.
+                // Call the final language's text file.
+                // Use the English translation to get the translation.
+                string midTranslation = ToEnglishTranslation(toBeTranslated, initialLanguage);
+                translatedMessage = FromEnglishTranslation(midTranslation, finalLanguage);
+            }
+
+            finalMessageBox.Text = translatedMessage;
+        }
+
+        // Cannot be used for screen initialization using language. Will be creating a method specific for phrases.
+        public string FromEnglishTranslation(string oldMessage, string newLanguage)
+        {
+            // Initialize final message.
+            string finalMessage = "";
+
+            // Split the old message into separate words in an array.
+            string[] oldMessageArray = oldMessage.Split();
+            int arrayLength = oldMessageArray.Length;
+
+            // Variables to the read the final language's text file.
+            string currentWord;
+            string line;
+            string[] twoLanguages;
+            char[] splitLine = { '|' };
+
+            // Go through each words in the oldMessageArray/.
+            for (int i = 0; i < arrayLength; i++)
+            {
+                currentWord = oldMessageArray[i].ToLower();
+
+                // Read the file.
+                System.IO.StreamReader file = new System.IO.StreamReader("Translation/" + newLanguage + ".txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    twoLanguages = line.Split(splitLine);
+
+                    if (twoLanguages[0] == currentWord)
+                    {
+                        finalMessage = finalMessage + twoLanguages[1] + " ";
+                    }
+                }
+                file.Close();
+                // Not sure what this is supposed to do.
+                System.Console.ReadLine();
+            }
+
+            // Return the final message.
+            return finalMessage;
+        }
+
+        public string ToEnglishTranslation(string oldMessage, string oldLanguage)
+        {
+            // Initialize final message.
+            string finalMessage = "";
+
+            // Split the old message into separate words in an array.
+            string[] oldMessageArray = oldMessage.Split();
+            int arrayLength = oldMessageArray.Length;
+
+            // Variables to the read the final language's text file.
+            string currentWord;
+            string line;
+            string[] twoLanguages;
+            char[] splitLine = { '|' };
+
+            // Go through each words in the oldMessageArray/.
+            for (int i = 0; i < arrayLength; i++)
+            {
+                currentWord = oldMessageArray[i].ToLower();
+
+                // Read the file.
+                System.IO.StreamReader file = new System.IO.StreamReader("Translation/" + oldLanguage + ".txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    twoLanguages = line.Split(splitLine);
+
+                    if (twoLanguages[1] == currentWord)
+                    {
+                        finalMessage = finalMessage + twoLanguages[0] + " ";
+                    }
+                }
+                file.Close();
+                // Not sure what this is supposed to do.
+                System.Console.ReadLine();
+            }
+
+            // Return the final message.
+            return finalMessage;
+        }
     }
 }
