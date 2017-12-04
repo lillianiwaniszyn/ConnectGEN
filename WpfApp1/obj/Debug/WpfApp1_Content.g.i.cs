510a9ec3f0b28b8e9dc83e701de10c6db33e28b5
KarlaMariann Juego <karlamariann.juego@ucalgary.ca>
2017-12-04 15:59:08 -0700

    Changed Appearance of AddImageMenu

--- WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
+++ WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("translation/spanish.txt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/karlajuego.jpg")]
 
 
