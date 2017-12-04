e9216429ba9a142ef7e6b89795fc46250b285c70
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2017-12-03 23:54:54 -0700

    more values initialized in arrays

--- WpfApp1/Login_Main.xaml.cs
+++ WpfApp1/Login_Main.xaml.cs
@@ -20,17 +20,33 @@ namespace WpfApp1
     public partial class Login_Main : Window
     {
         public Login_Main()
-        {
+        { //add more later... will be tedious...
             Global_Data.existing_Usernames[0] = "liwanisz";
-            Global_Data.existing_Usernames[1] = "Lillian";
+            Global_Data.existing_Usernames[1] = "Karla1";
             Global_Data.existing_Usernames[2] = "cora";
-            Global_Data.existing_Usernames[3] = "Cora";
-            Global_Data.existing_Usernames[4] = "Cora1";
+            Global_Data.existing_Usernames[3] = "Simon22";
+            Global_Data.existing_Usernames[4] = "MalikBrown";
             Global_Data.passwords[0] = "1234";
             Global_Data.passwords[1] = "3245";
             Global_Data.passwords[2] = "0000";
             Global_Data.passwords[3] = "1111";
             Global_Data.passwords[4] = "3333";
+            Global_Data.firstnames[0] = "Lillian";
+            Global_Data.firstnames[1] = "Karla";
+            Global_Data.firstnames[2] = "Cora";
+            Global_Data.firstnames[3] = "Simon";
+            Global_Data.firstnames[4] = "Malik";
+            Global_Data.lastnames[0] = "Iwaniszyn";
+            Global_Data.lastnames[1] = "Juego";
+            Global_Data.lastnames[2] = "Brown";
+            Global_Data.lastnames[3] = "Cowell";
+            Global_Data.lastnames[4] = "Brown";
+            Global_Data.contacts[0,0] = "Karla1"; //this shows us that Karla is in Lillian's contacts
+            Global_Data.contacts[0, 1] = "Simon22"; // simon is also Lillian's friend
+            Global_Data.contacts[1, 0] = "liwanisz"; // lillian is Karla's friend
+            Global_Data.contacts[3, 0] = "liwnaisz"; //lillian is simons friend
+            //... and so on and so forth....
+
             InitializeComponent();
         }
         void OnClickA(object sender, RoutedEventArgs e)
