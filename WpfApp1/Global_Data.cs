19f9b63e5bb45844662636b30ecb7c2fcd04c619
hanum-lee <chococloudy@gmail.com>
2017-12-04 19:12:25 -0700

    Global Data edited

--- WpfApp1/Global_Data.cs
+++ WpfApp1/Global_Data.cs
@@ -24,6 +24,35 @@ namespace WpfApp1
         {
             languageChosen = chosen;
         }
+
+        public Global_Data()
+        {
+            existing_Usernames[0] = "liwanisz";
+            existing_Usernames[1] = "Karla1";
+            existing_Usernames[2] = "cora";
+            existing_Usernames[3] = "Simon22";
+            existing_Usernames[4] = "MalikBrown";
+            passwords[0] = "1234";
+            passwords[1] = "3245";
+            passwords[2] = "0000";
+            passwords[3] = "1111";
+            passwords[4] = "3333";
+            firstnames[0] = "Lillian";
+            firstnames[1] = "Karla";
+            firstnames[2] = "Cora";
+            firstnames[3] = "Simon";
+            firstnames[4] = "Malik";
+            lastnames[0] = "Iwaniszyn";
+            lastnames[1] = "Juego";
+            lastnames[2] = "Brown";
+            lastnames[3] = "Cowell";
+            lastnames[4] = "Brown";
+            contacts[0, 0] = "Karla1"; //this shows us that Karla is in Lillian's contacts
+            contacts[0, 1] = "Simon22"; // simon is also Lillian's friend
+            contacts[1, 0] = "liwanisz"; // lillian is Karla's friend
+            contacts[3, 0] = "liwnaisz"; //lillian is simons friend
+
+        }
     }
 
 
