99ba71a7765964927e06d2d63c62dfbe9c61e192
Daniel Lee <chococloudy@gmail.com>
2017-12-05 22:09:06 -0700

    Calls with the cases of no answer are added

---
+++ WpfApp1/AudioCallNoAnswer.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for AudioCallNoAnswer.xaml
+    /// </summary>
+    public partial class AudioCallNoAnswer : Window
+    {
+        DispatcherTimer _timer;
+        TimeSpan _time;
+        TimeSpan waitTime;
+        public AudioCallNoAnswer()
+        {
+            InitializeComponent();
+        }
+    }
+}
